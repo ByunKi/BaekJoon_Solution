248690deb221b11da2bbb07c4596442befac825a
HoneyBee <79976395+kryowen@users.noreply.github.com>
2021-09-07 17:48:14 +0900

    ..

---
+++ C/17295.c
@@ -0,0 +1,21 @@
+/*
+제출 번호 33089587
+아이디: adviate	
+문제: 17295
+결과: 맞았습니다!!
+메모리: 1116KB
+시간: 0ms
+언어: C99 
+코드 길이: 115B
+제출한 시간: 2021.09.07 17:46:14
+*/
+
+#include <stdio.h>
+
+int main() {
+	char str[500];
+	scanf("%s", &str);
+	printf("Avengers: Endgame \n");
+
+	return 0;
+}

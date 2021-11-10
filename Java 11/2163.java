e96cd448d9aad8ec2f4970016bf11be2a94a3040
HoneyBee <79976395+kryowen@users.noreply.github.com>
2021-11-10 12:46:29 +0900

    ..

---
+++ Java 11/2163.java
@@ -0,0 +1,24 @@
+// 제출 번호: 35269224
+// 아이디: adviate
+// 문제: 2163
+// 문제 제목: 초콜릿 자르기
+// 결과: 맞았습니다!!
+// 메모리: 17676KB
+// 시간: 204ms
+// 언어: Java 11
+// 코드 길이: 272B
+// 제출한 시간: 2021.11.10 12:44:48
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println(n * m - 1);
+
+        scanner.close();
+    }
+}

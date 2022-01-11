a0125dd05a51d9272bbeb7bfec5ed96f875f11df
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-01-11 15:13:44 +0900

    ..

---
+++ Java 11/24082.java
@@ -0,0 +1,22 @@
+/*
+ * 제출 번호: 37384629
+ * ID: adviate
+ * 문제: 24082
+ * 결과: 맞았습니다!!
+ * 메모리: 17692 KB
+ * 시간: 204 ms
+ * 언어: Java 11
+ * 코드 길이: 234 B
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println((int) Math.pow(input, 3));
+	}
+}

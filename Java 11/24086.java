aec18fb398f603979f629b20e35c2b5709dc2972
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-01-11 15:49:39 +0900

    ..

---
+++ Java 11/24086.java
@@ -0,0 +1,23 @@
+/*
+ * 제출 번호: 37387021
+ * ID: adviate
+ * 문제: 24086
+ * 결과: 맞았습니다!!
+ * 메모리: 17672 KB
+ * 시간: 204 ms
+ * 언어: Java 11
+ * 코드 길이: 260 B
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int inputA = scanner.nextInt();
+		int inputB = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(inputB - inputA);
+	}
+}

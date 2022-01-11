ae2213c713654135632a2f6e9bb76814100d742b
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-01-11 15:03:42 +0900

    ..

---
+++ Java 11/24078.java
@@ -0,0 +1,22 @@
+/*
+ * 제출 번호: 37383963
+ * ID: adviate
+ * 문제: 24078
+ * 결과: 맞았습니다!!
+ * 메모리: 17640 KB
+ * 시간: 208 ms
+ * 언어: Java 11
+ * 코드 길이: 218 B
+ */
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(input%21);
+	}
+}

f1daac158a5d9eaa37b9ad1f566a864c563801a5
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-02-10 16:53:23 +0900

    ..

---
+++ PY/6810.py
@@ -0,0 +1,12 @@
+# 제출 번호: 38863857
+# 문제: 6810
+# 메모리: 30860 KB
+# 시간: 68 ms
+# 언어: Python3
+# 코드 길이: 95 B
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print('The 1-3-sum is', 91 + a + b * 3 + c)

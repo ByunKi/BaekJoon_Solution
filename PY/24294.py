506a5726bb81f0dd0d6c8adf0b318e567d1b00a6
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-02-10 18:18:18 +0900

    ..

---
+++ PY/24294.py
@@ -0,0 +1,12 @@
+# 제출 번호: 38869162
+# 문제: 24294
+# 메모리: 30860 KB
+# 시간: 72 ms
+# 언어: Python3
+# 코드 길이: 108 B
+
+w1 = int(input())
+h1 = int(input())
+w2 = int(input())
+h2 = int(input())
+print(4 + 2*max(w1,w2) + 2 *(h1+h2))

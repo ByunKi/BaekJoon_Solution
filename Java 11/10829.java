b8dfd81eb01cb57171607773d44df4c2da81d6ad
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-02-17 13:58:27 +0900

    ..

---
+++ Java 11/10829.java
@@ -0,0 +1,22 @@
+/*
+ * 제출 번호: 39203411
+ * ID: adviate
+ * 문제: 10829
+ * 결과: 맞았습니다!!
+ * 메모리: 14440 KB
+ * 시간: 124 ms
+ * 언어: Java 11
+ * 코드 길이: 296 B
+ */
+
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		BigInteger N = new BigInteger(reader.readLine());
+		System.out.println(N.toString(2));
+	}
+}

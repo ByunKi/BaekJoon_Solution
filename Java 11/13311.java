54e3b444e27a3ed4ec027ccda89035904295c2d3
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-02-21 11:55:13 +0900

    (a - 1) % a == -1

--- Java 11/13311.java
+++ Java 11/13311.java
@@ -14,7 +14,6 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    
 		System.out.println(-1);
 	}
 }

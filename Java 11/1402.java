d8b50f4ad91f60d6ae12ba3c42480463549c1bc6
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-02-24 08:07:00 +0900

    ..

---
+++ Java 11/1402.java
@@ -0,0 +1,26 @@
+/*
+ * 제출 번호: 39533157
+ * ID: adviate
+ * 문제: 1402
+ * 결과: 맞았습니다!!
+ * 메모리: 14252 KB
+ * 시간: 128 ms
+ * 언어: Java 11
+ * 코드 길이:  B
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int TC = Integer.parseInt(reader.readLine());
+		for(int i = 0; i < TC; i++) {
+			String[] input = reader.readLine().split(" ");
+			int A = Integer.parseInt(input[0]), B = Integer.parseInt(input[1]);
+			
+			System.out.println("yes");
+		}
+	}
+}

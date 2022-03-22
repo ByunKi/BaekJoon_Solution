7c3e07021174fe0e1aebe33130b46821ad9b60d2
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-03-23 00:57:44 +0900

    ..

---
+++ Java 11/16485.java
@@ -0,0 +1,23 @@
+/*
+ * 제출 번호: 40847793
+ * ID: adviate
+ * 문제: 16485
+ * 결과: 맞았습니다!!
+ * 메모리: 14252 KB
+ * 시간: 128 ms
+ * 언어: Java 11
+ * 코드 길이: 337 B
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = reader.readLine().split(" ");
+		double A = Double.valueOf(input[0]), B = Double.valueOf(input[1]);
+		
+		System.out.println(A / B);
+	}
+}

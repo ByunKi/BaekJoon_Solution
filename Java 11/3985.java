97123afdeca1dddf1f3117496025dbbfdd9906db
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-03-31 00:59:09 +0900

    ..

--- Java 11/3985.java
+++ Java 11/3985.java
@@ -1,52 +1,35 @@
 /*
- * 제출 번호: 41244689
+ * 제출 번호: 41246529
  * ID: adviate
  * 문제: 3985
  * 결과: 맞았습니다!!
- * 메모리: 14416 KB
- * 시간: 140 ms
+ * 메모리: 14824 KB
+ * 시간: 136 ms
  * 언어: Java 11
- * 코드 길이: 1100 B
+ * 코드 길이: 827 B
  */
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int L = Integer.valueOf(reader.readLine()), N = Integer.valueOf(reader.readLine());
+		int[] cake = new int[L];
 		
-		int len = Integer.parseInt(br.readLine());
-		int[] cake = new int[len + 1];
-		int N = Integer.parseInt(br.readLine());
-		
-		int[][] arr = new int[N][2];
-		int expect = 0;
-		int maxIdx = 0;
-		int maxVal = 0;
-		for(int i=0; i<N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			arr[i][0] = start;
-			arr[i][1] = end;
-			if(arr[expect][1] - arr[expect][0] < end - start)
-				expect = i;
-			
-			int count = 0;
-			for(int j=0; j<end - start + 1; j++) {
-				if(cake[start + j] == 0) {
-					cake[start + j] = i+1;
-					count++;
-				}
+		int maxSum = 0, maxExpect = 0, expectIdx = 0, realIdx = 0;
+		for(int i = 0; i < N; i++) {
+			String[] input = reader.readLine().split(" ");
+			int begin = Integer.valueOf(input[0]), end = Integer.valueOf(input[1]), sum = 0;
+			if(end - begin > maxExpect) { maxExpect = end - begin; expectIdx = i; }
+
+			for(int j = begin - 1; j < end; j++) {
+				if(cake[j] == 0) { cake[j] = (i + 1); sum++; }
 			}
-			if(count > maxVal) {
-				maxVal = count;
-				maxIdx = i;
-			}
+			if(sum > maxSum) { maxSum = sum; realIdx = i;}
 		}
-		
-		System.out.println(expect+1);
-		System.out.println(maxIdx+1);
+		System.out.println(expectIdx + 1);
+		System.out.println(realIdx + 1);
 	}
 }

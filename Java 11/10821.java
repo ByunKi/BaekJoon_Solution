35bc590b76eccdd1ea39d4be4cacbb67db003ba7
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-04-06 11:03:06 +0900

    ..

---
+++ Java 11/10821.java
@@ -0,0 +1,21 @@
+/*
+ * 제출 번호: 41571409
+ * ID: adviate
+ * 문제: 10821
+ * 결과: 맞았습니다!!
+ * 메모리: 14248 KB
+ * 시간: 124 ms
+ * 언어: Java 11
+ * 코드 길이: 271 B
+ */
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = reader.readLine().split(",");
+		System.out.println(input.length);
+	}
+}

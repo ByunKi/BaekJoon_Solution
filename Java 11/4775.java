0ad37e910bbea1db1ce0cfcd8dc1778c3199e279
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-05-08 07:21:15 +0900

    ..

--- Java 11/4775.java
+++ Java 11/4775.java
@@ -1,12 +1,12 @@
 /*
- * 제출 번호: 42982852
+ * 제출 번호: 42982872
  * ID: adviate
  * 문제: 4775
  * 결과: 맞았습니다!!
- * 메모리: 23984 KB
- * 시간: 292 ms
+ * 메모리: 24148 KB
+ * 시간: 284 ms
  * 언어: Java 11
- * 코드 길이: 923 B
+ * 코드 길이: 919 B
  */
 
 import java.io.*;
@@ -22,21 +22,17 @@ public class Main {
 
 		int TC = Integer.valueOf(reader.readLine());
 		for(int i = 0; i < TC; i++) {
-			String input = "";
-			Boolean flag = true;
-			StringBuilder ans = new StringBuilder();
+			ArrayList<String> list = new ArrayList<>();
 			
+			String input;
 			while(!(input = reader.readLine()).equals("-1")) {
-				if(!set.contains(input)) { 
-					flag = false; 
-					ans.append(input + "\n"); 
-				}
+				if(!set.contains(input)) { list.add(input); }
 			}
 			
-			if(flag) { System.out.println("Email " + (i + 1) + " is spelled correctly."); } 
+			if(list.size() < 1) { System.out.println("Email " + (i + 1) + " is spelled correctly."); } 
 			else {
 				System.out.println("Email " + (i + 1) + " is not spelled correctly.");
-				System.out.print(ans);
+				for(int j = 0; j < list.size(); j++) { System.out.println(list.get(j)); }
 			}
 		}
 		System.out.println("End of Output");

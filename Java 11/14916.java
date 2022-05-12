427beb622d2165f90c3e5007ebcf3d6d82b6ba15
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-05-13 01:22:15 +0900

    DP

--- Java 11/14916.java
+++ Java 11/14916.java
@@ -1,12 +1,12 @@
 /*
- * 제출 번호: 43223372
+ * 제출 번호: 43223612
  * ID: adviate
  * 문제: 14916
  * 결과: 맞았습니다!!
- * 메모리: 14204 KB
- * 시간: 124 ms
+ * 메모리: 14228 KB
+ * 시간: 120 ms
  * 언어: Java 11
- * 코드 길이: 422 B
+ * 코드 길이: 447 B
  */
 
 import java.io.*;
@@ -16,17 +16,8 @@ public class Main {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.valueOf(reader.readLine());
-		if(N != 3 && N != 1) {
-			int F = N / 5, T = N % 5;
-			
-			while(T % 2 != 0) {
-				F--;
-				T += 5;
-			}
-			
-			System.out.println(F + (T / 2));
-		} else {
-			System.out.println(-1);
-		}
+		if(N == 1 || N == 3) { System.out.println(-1); }
+		else if((N % 5) % 2 == 0) { System.out.println((N / 5) + ((N % 5) / 2) ); }
+		else if((N % 5) % 2 != 0) { System.out.println((N / 5 - 1) + ((N % 5 + 5) / 2)); }
 	}
 }

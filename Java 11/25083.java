7761f8f47b6356e7f33dc5dbec257593e13003fd
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-05-14 10:29:06 +0900

    ..

---
+++ Java 11/25083.java
@@ -0,0 +1,23 @@
+/*
+ * 제출 번호: 43271921
+ * ID: adviate
+ * 문제: 25083
+ * 결과: 맞았습니다!!
+ * 메모리: 13884 KB
+ * 시간: 120 ms
+ * 언어: Java 11
+ * 코드 길이: 258 B
+ */
+
+
+public class Main {
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" + 			
+				   "      |";
+		
+		System.out.print(s);
+	

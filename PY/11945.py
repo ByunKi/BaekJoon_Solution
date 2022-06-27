561d1769b9f4e7ed55d85ce6852552d5ef2e490b
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-06-27 12:26:59 +0900

    ..

---
+++ PY/11945.py
@@ -0,0 +1,14 @@
+"""
+ * 제출 번호: 45070717
+ * ID: adviate
+ * 문제: 11945
+ * 결과: 맞았습니다!!
+ * 메모리: 30840 KB
+ * 시간: 68 ms
+ * 언어: Python 3
+ * 코드 길이: 82 B
+"""
+
+a, b = list(map(int, input().split()))
+for _ in range(a):
+    print(input()[::-1])

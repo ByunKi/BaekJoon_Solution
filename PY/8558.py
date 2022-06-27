3ece8dc44ce9ac9b5110876e2e2085b144228d3d
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-06-27 12:39:41 +0900

    ..
    내장함수가 있네..

---
+++ PY/8558.py
@@ -0,0 +1,13 @@
+"""
+ * 제출 번호: 45071118
+ * ID: adviate
+ * 문제: 8558
+ * 결과: 맞았습니다!!
+ * 메모리: 32952 KB
+ * 시간: 84 ms
+ * 언어: Python 3
+ * 코드 길이: 62 B
+"""
+
+from math import factorial
+print(factorial(int(input())) % 10)

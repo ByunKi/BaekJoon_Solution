ac2459dabc1335ec57b0960fbd3289ed730fd2b7
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-07-03 10:56:53 +0900

    ..

--- PY/3135.py
+++ PY/3135.py
@@ -1,15 +1,14 @@
 """
- * 제출 번호: 45411372
+ * 제출 번호: 45411535
  * ID: adviate
  * 문제: 3135
  * 결과: 맞았습니다!!
  * 메모리: 30840 KB
  * 시간: 68 ms
  * 언어: Python 3
- * 코드 길이: 188 B
+ * 코드 길이: 130 B
 """
 
 A, B = map(int, input().split())
 arr = [abs(B - int(input())) for _ in range(int(input()))]
-cnt = 1
-print(cnt + min(abs(A - B)-1, min(arr)))
+print(1 + min(abs(A - B)-1, min(arr)))

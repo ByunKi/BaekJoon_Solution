6aff20939b0e40e57bee6e7f4679d9e47e11c24a
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-07-10 12:38:29 +0900

    ..

--- PY/10825.py
+++ PY/10825.py
@@ -1,13 +1,16 @@
 """
- * 제출 번호: 45809594
+ * 제출 번호: 45809837
  * ID: adviate
  * 문제: 10825
  * 결과: 맞았습니다!!
- * 메모리: 62880 KB
- * 시간: 4496 ms
+ * 메모리: 61856 KB
+ * 시간: 436 ms
  * 언어: Python 3
- * 코드 길이: 220 B
+ * 코드 길이: 242 B
 """
+
+import sys
+input = sys.stdin.readline
 
 arr = []
 for _ in range(int(input())):
@@ -15,5 +18,5 @@ for _ in range(int(input())):
     arr.append([name, int(K), int(E), int(M)])
 arr.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
 
-for i in range(len(arr)):
-    print(arr[i][0])
+for i in arr:
+    print(i[0])

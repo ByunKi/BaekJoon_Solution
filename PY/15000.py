773b9aa61d3ca1af65c405524ccc85f86fa21dc5
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-08-30 01:02:49 +0900

    ..

---
+++ PY/15000.py
@@ -0,0 +1,15 @@
+"""
+제출 번호: 48477188
+아이디: adviate
+문제: 15000
+결과: 맞았습니다!!
+메모리: 32796 KB 
+시간: 72 ms
+언어: Python 3
+코드 길이: 85 B
+"""
+
+from sys import stdin
+def ip(): return stdin.readline().rstrip()
+
+print(ip().upper())

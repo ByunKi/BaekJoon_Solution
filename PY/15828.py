537ea8829828cb051af756cbfd1759326d32e4f2
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-09-17 03:29:46 +0900

    ..

--- PY/15828.py
+++ PY/15828.py
@@ -1,22 +1,26 @@
 """
- * 제출 번호: 49248896
+ * 제출 번호: 49248960
  * ID: adviate
  * 문제: 15828
  * 결과: 100점
  * 메모리: 36060 KB
- * 시간: 976 ms
+ * 시간: 960 ms
  * 언어: Python 3
- * 코드 길이: 239 B
+ * 코드 길이: 286 B
 """
 
 from sys import stdin
 def ip(): return stdin.readline().rstrip()
 
-b = []
+b, s = [], 0
 N = int(ip())
 while 1:
     i = ip()
     if i == '-1': break
-    if len(b) < N and i != '0': b.append(i)
-    if i == '0': b.pop(0)
+    if s < N and i != '0':
+        s += 1
+        b.append(i)
+    if i == '0':
+        del b[0]
+        s -= 1
 print(' '.join(b) if b else 'empty')

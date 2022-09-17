e117c0e6eed82f17efa2c11135bb91f102c96dcf
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-09-18 00:16:56 +0900

    ..

--- PY/25497.py
+++ PY/25497.py
@@ -1,35 +1,24 @@
 """
- * 제출 번호: 49287173
+ * 제출 번호: 49287458
  * ID: adviate
  * 문제: 25497
  * 결과: 맞았습니다!!
- * 메모리: 31716 KB
- * 시간: 140 ms
+ * 메모리: 30840 KB
+ * 시간: 136 ms
  * 언어: Python 3
- * 코드 길이: 491 B
+ * 코드 길이: 327 B
 """
 
 from sys import stdin
 def ip(): return stdin.readline().rstrip()
 
+c = 0
+dic = {'L':0, 'S':0}
 _ = ip()
-s = ip()
-ls, ss, c = [], [], 0
-for i in s:
+for i in ip():
     if i.isdigit(): c += 1
-    else:
-        if i == 'L': ls.append(i)
-        if i == 'S': ss.append(i)
-        if i == 'R':
-            if ls:
-                c += 1
-                del ls[-1]
-            else:
-                break
-        if i == 'K':
-            if ss:
-                c += 1
-                del ss[-1]
-            else:
-                break
+    elif i == 'L' or i == 'S': dic[i] += 1
+    elif i == 'R' and dic['L'] > 0: c += 1; dic['L'] -= 1
+    elif i == 'K' and dic['S'] > 0: c += 1; dic['S'] -= 1
+    else: break
 print(c)

75c434d89585e2301c33b25f26ec55db3c74c1c6
HoneyBee <79976395+kryowen@users.noreply.github.com>
2022-09-19 00:57:28 +0900

    ..

---
+++ PY/2910.py
@@ -0,0 +1,19 @@
+"""
+제출 번호: 49332055
+아이디: adviate
+문제: 2910
+결과: 맞았습니다!!
+메모리: 30840 KB 
+시간: 72 ms
+언어: Python 3
+코드 길이: 209 B
+"""
+
+from sys import stdin
+def ip(): return stdin.readline().rstrip()
+
+dic = {}
+for _ in range(int(ip())):
+    i = tuple(sorted(list(ip().split())))
+    dic[i] = dic[i]+1 if i in dic else 1
+print(max(dic.values()))
